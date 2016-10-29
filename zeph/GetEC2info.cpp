fa1ffec48a65ac2bf3bd8c0a9fc5caf0059b83d6
Zeph Grunschlag <zgrunschlag@gmail.com>
2016-10-28 21:08:43 -0400

    Trying to add a new sub-project for my own little tests

---
+++ zeph/GetEC2info.cpp
@@ -0,0 +1,12 @@
+//
+// Created by zeph on 10/28/16.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "This program will test connectivity the EC2 boxes on the 'tzaffi' account";
+
+    return 0;
+}
